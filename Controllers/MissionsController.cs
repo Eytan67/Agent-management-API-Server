e1b4ad97f80ee3acc260b9048c7fac9f24105b66
Eytan zichel <eytanzichel@gmail.com>
2024-08-22 11:58:12 +0300

    mission controler

--- Controllers/MissionsController.cs
+++ Controllers/MissionsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AgentManagementAPIServer.Enums;
+using AgentManagementAPIServer.Intrfaces;
+using AgentManagementAPIServer.Models;
+using AgentManagementAPIServer.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgentManagementAPIServer.Controllers
@@ -7,5 +11,34 @@ namespace AgentManagementAPIServer.Controllers
     [ApiController]
     public class MissionsController : ControllerBase
     {
+        private readonly MissionsService _missionsService;
+        public MissionsController(IService<Mission> missionsService)
+        {
+            this._missionsService = missionsService as MissionsService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var missions = await _missionsService.GetAllAsync();
+
+            return StatusCode(StatusCodes.Status200OK, new { missions });
+        }
+
+        [HttpPost("update")]
+        public async Task<IActionResult> updateMissionsProgres()
+        {
+            var activMissions = _missionsService.updateMissionsAsync();
+
+            return StatusCode(StatusCodes.Status200OK);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> CommandForMission([FromQuery] int id, [FromBody] EMissionsStatus status)
+        {
+            await _missionsService.UpdateStatusAsync(id, status);
+
+            return StatusCode(StatusCodes.Status200OK);
+        }
     }
 }
