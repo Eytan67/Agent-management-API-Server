135cb5c4f8cb603bbc2eae375293305b12f2e62b
Eytan zichel <eytanzichel@gmail.com>
2024-08-25 13:28:10 +0300

    new functions for move logic

--- Shared/MoveLogic.cs
+++ Shared/MoveLogic.cs
@@ -5,49 +5,27 @@ namespace AgentManagementAPIServer.Shared
 {
     public class MoveLogic
     {
-        public static Coordinates NextLocation(Coordinates location, EDirection direction)
+        public static Coordinates NextLocationByDirection(Coordinates location, string direction)
         {
-            //throw an eror if it out of table
-            Coordinates newLocation = new Coordinates();
-            switch (direction)
+            Coordinates vector = DirectionDictionary[direction];
+            return NextLocation(location, vector);
+
+        }
+        public static Coordinates NextLocationByCoordinates(Coordinates location, Coordinates targetLocation)
+        {
+            var vector = DefineVector(location, targetLocation);
+            return NextLocation(location, vector);
+        }
+        public static Coordinates NextLocation(Coordinates location, Coordinates vector)
+        {
+            Coordinates newLocation = location + vector;
+            if (!IsInRange(newLocation))
             {
-                case EDirection.s:
-                    newLocation.X = location.X;
-                    newLocation.Y = location.Y+1;
-                    break;
-                default:
-                    break;
+                //throw in valid move
+                return location;
             }
 
             return newLocation;
-        }
-        public static EDirection ChoosDirection(Coordinates agentLocation, Coordinates targetLocation)
-        {
-            EDirection direction;
-            if(agentLocation.X != targetLocation.X && agentLocation.Y != targetLocation.Y)
-            {
-                if (agentLocation.X < targetLocation.X)
-                {
-                    direction = agentLocation.Y < targetLocation.Y ? EDirection.ne : EDirection.se;
-                }
-                else
-                {
-                    direction = agentLocation.Y < targetLocation.Y ? EDirection.nw : EDirection.sw;
-                }
-            }
-            else
-            {
-                if(agentLocation.X != targetLocation.X)
-                {
-                    direction = agentLocation.X < targetLocation.X ? EDirection.e : EDirection.w;
-                }
-                else
-                {
-                    direction = agentLocation.Y < targetLocation.Y ? EDirection.n : EDirection.s;
-                }
-            }
-            return direction;
-
         }
         public static bool IsDistanceAppropriate(Coordinates agentLocation, Coordinates targetLocation)
         {
@@ -55,5 +33,31 @@ namespace AgentManagementAPIServer.Shared
 
             return distance < 200;
         }
+        private static bool IsInRange(Coordinates coordinates)
+        {
+            return coordinates.X <= 1000
+                && coordinates.Y <= 1000
+                && coordinates.X >= 0
+                && coordinates.Y >= 0;
+        }
+        private static Coordinates DefineVector(Coordinates agentPoints, Coordinates targetPoints)
+        {
+            var vector = targetPoints - agentPoints;
+            vector.X = vector.X > 0 ? 1 : -1;
+            vector.Y = vector.Y > 0 ? 1 : -1;
+            return vector;
+        }
+        private static Dictionary<string, Coordinates> DirectionDictionary
+    = new Dictionary<string, Coordinates>()
+    {
+                { "n", new Coordinates(0, 1) },
+                { "ne", new Coordinates(1, 1) },
+                { "e", new Coordinates(1, 0) },
+                { "se", new Coordinates(1, -1) },
+                { "s", new Coordinates(0, -1) },
+                { "sw", new Coordinates(-1, -1) },
+                { "w", new Coordinates(-1, 0) },
+                { "nw", new Coordinates(-1, 1) }
+    };
     }
 }
