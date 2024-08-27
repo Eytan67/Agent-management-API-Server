9fb3c6be92f6089f0d9326db90bc8a3c88d6d685
Eytan zichel <eytanzichel@gmail.com>
2024-08-27 10:35:24 +0300

    modal

---
+++ Shared/FullMission.cs
@@ -0,0 +1,11 @@
+﻿using AgentManagementAPIServer.Models;
+
+namespace AgentManagementAPIServer.Shared
+{
+    public class FullMission
+    {
+        public Mission Mission { get; set; }
+        public Agent Agent { get; set; }
+        public Target Target { get; set; }
+    }
+}
